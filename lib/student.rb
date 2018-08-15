16dc09cb77c593781517b09d23f928cb93cfb945
Amee Covarrubias <ameecova@yahoo.com>
2018-08-15 07:02:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,7 @@
 class Student < User
-  attr_reader :knowledge
   
   def initialize(knowledge)
-    @knowledge = knowledge
     knowledge = []
   end
   
-  def learn(string)
-    KNOWLEDGE << string
-  end 
-
 end
